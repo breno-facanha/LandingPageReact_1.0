7a0adb6a9c317097dd02213596c8555d86f9781f
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2023-04-26 23:03:27 -0300

    Finalizando a primeira parte do componente Main
    Colocado img e animação
    e  a apresentação e alinhado tudo com display flex

--- landing-page/src/components/Main/Main.jsx
+++ landing-page/src/components/Main/Main.jsx
@@ -1,11 +1,29 @@
+import styles from './Main.module.css'
+import photo from '../../assets/breno.png'
+
 export default function Main(){
     return (
         <>
-            <div className="QuemSouEu"></div>
+        <div className={styles.main}>
+            <div className={styles.QuemSouEu}>
+                <div className="nomeDev">
+                    <p>Olá seja bem vindo{'(a)'}!</p> 
+                    <p>Me Chamo: Breno Façanha.</p>  
+                    <p>Sou estudante de desenvolvimento web</p>    
+                </div>
+
+                <div className={styles.imgBreno}>
+                    <img src={photo} alt="" />
+                </div>
+            </div>
 
             <div className="Cards">
-
+                <div className="formacao">teste</div>
+                <div className="tecnologias"></div>
+                <div className="projetos"></div>
             </div>
+        </div>
+           
         </>
     )
 }
--- landing-page/src/main.jsx
+++ landing-page/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
 import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Main />
   </React.StrictMode>,
 )
