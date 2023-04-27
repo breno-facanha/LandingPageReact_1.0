a5a2f03a5b085a57862f21cc318bd33e31ad2c99
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2023-04-26 23:52:48 -0300

    adicionado Imagens e hover nos cards

--- landing-page/src/components/Main/Main.jsx
+++ landing-page/src/components/Main/Main.jsx
@@ -1,5 +1,9 @@
 import styles from './Main.module.css'
-import photo from '../../assets/breno.png'
+import photoBreno from '../../assets/breno.png'
+import photoGitHub from '../../assets/github.png'
+import photoGraduate from '../../assets/graduate.png'
+import photoProject from '../../assets/project.png'
+import photoList from '../../assets/list.png'
 
 export default function Main(){
     return (
@@ -12,17 +16,19 @@ export default function Main(){
                 </div>
 
                 <div className={styles.imgBreno}>
-                    <img src={photo} alt="" />
+                    <img src={photoBreno} alt="" />
                 </div>
             </div>
 
             <div className={styles.cards}>
                 <div className={styles.formacao}>
+                    <img src={photoGraduate} alt="" />
                     <span>FORMAÇÃO</span>
                     <p>ANALISE E DESENVOLVIMENTO DE SISTEMAS</p>
                 </div>
 
                 <div className={styles.tecnologias}>
+                    <img src={photoList} alt="" />
                     <span>TECNOLOGIAS</span>  
                     <p>HTML</p>
                     <p>CSS</p>
@@ -31,6 +37,7 @@ export default function Main(){
                 </div>
 
                 <div className={styles.projetos}>
+                    <img src={photoProject} alt="" />
                     <span>PROJETOS</span> 
                     <a href="https://breno-facanha.github.io/projeto-explorer-013/" target="__blank"><p>FOCUS TIMER</p></a>
                     <a href="#"><p>LANDING-PAGE</p></a>
