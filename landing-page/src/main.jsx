a88d06fc0c4138f1b47d9ec5dc112153aaac7a6e
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2023-04-26 22:16:33 -0300

    Ajustes de configurações do projeto
    alterações
    - index.html para pt-br
    - css global resetado as configurações de box
    - importando componente header no main jsx

--- landing-page/src/main.jsx
+++ landing-page/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
+import Header from './components/Header/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <Header />
   </React.StrictMode>,
 )
