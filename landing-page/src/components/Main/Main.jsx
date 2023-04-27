5082cb9fe3774b9a87c047ff35c07268eb1f639c
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2023-04-26 23:37:14 -0300

    Finalizando main com os cards
    Adicionando cards de formação tecnologias e projetos e com algumas animações em css

--- landing-page/src/components/Main/Main.jsx
+++ landing-page/src/components/Main/Main.jsx
@@ -3,7 +3,6 @@ import photo from '../../assets/breno.png'
 
 export default function Main(){
     return (
-        <>
         <div className={styles.main}>
             <div className={styles.QuemSouEu}>
                 <div className="nomeDev">
@@ -17,13 +16,27 @@ export default function Main(){
                 </div>
             </div>
 
-            <div className="Cards">
-                <div className="formacao">teste</div>
-                <div className="tecnologias"></div>
-                <div className="projetos"></div>
+            <div className={styles.cards}>
+                <div className={styles.formacao}>
+                    <span>FORMAÇÃO</span>
+                    <p>ANALISE E DESENVOLVIMENTO DE SISTEMAS</p>
+                </div>
+
+                <div className={styles.tecnologias}>
+                    <span>TECNOLOGIAS</span>  
+                    <p>HTML</p>
+                    <p>CSS</p>
+                    <p>JAVASCRIPT</p>
+                    <p>REACTJS</p>
+                </div>
+
+                <div className={styles.projetos}>
+                    <span>PROJETOS</span> 
+                    <a href="https://breno-facanha.github.io/projeto-explorer-013/" target="__blank"><p>FOCUS TIMER</p></a>
+                    <a href="#"><p>LANDING-PAGE</p></a>
+                    <a href="https://breno-facanha.github.io/Suporte-Fribal/" target="__blank"><p>SUPORTE</p></a>
+                </div>
             </div>
         </div>
-           
-        </>
     )
 }
