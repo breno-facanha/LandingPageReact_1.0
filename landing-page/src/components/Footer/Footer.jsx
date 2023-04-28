582f62f338cceec61502cbb5eca37c6965dc4af8
Breno Façanha <61479511+breno-facanha@users.noreply.github.com>
2023-04-27 22:51:29 -0300

    Finalizado o componente Footer
    Linkando minhas redes socias linkedn, github e email
    Colocando animações nos links

--- landing-page/src/components/Footer/Footer.jsx
+++ landing-page/src/components/Footer/Footer.jsx
@@ -6,10 +6,24 @@ import styles from './Footer.module.css'
 
 export default function Footer(){
     return (
-        <div className='contatos'>
-            <a href=""> <img src={photoLinkedin} alt="" />Linkedin</a>
-            <a href=""> <img src={photoGitHub} alt="" />GitHub</a>
-            <a href=""> <img src={photoGmail} alt="" />Email</a>
+        <div className={styles.contatos}>
+            <a 
+                href="https://www.linkedin.com/in/breno-fa%C3%A7anha-5a57a3b6/" 
+                    target="__blank"> 
+                <img src={photoLinkedin} alt="" />
+            </a>
+            
+            <a 
+                href="https://github.com/breno-facanha"
+                    target="___blank"> 
+                <img src={photoGitHub} alt="" />
+            </a>
+
+            <a 
+                href="mailto:brenofacanha.ti@gmail.com"
+                    target="___blank"> 
+                <img src={photoGmail} alt="" /></a>
         </div>
     )
 }
+
